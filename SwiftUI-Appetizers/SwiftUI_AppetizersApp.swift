6e7da318ed4bef368f4275ddf9be36ebeb4f871a
Daniel Karath <karath.dani@gmail.com>
2023-02-06 14:11:41 +0100

    Initial Commit

---
+++ SwiftUI-Appetizers/SwiftUI_AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppetizersApp.swift
+//  SwiftUI-Appetizers
+//
+//  Created by Daniel Karath on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
